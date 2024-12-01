a32a6f9facec86d4adf4b86a4f5fa7ea2bf89203
Tony Melvin <webn1008@gmail.com>
2024-12-01 16:53:20 -0500

    Update code to add the refresh rate

--- main.py
+++ main.py
@@ -2,26 +2,25 @@ import pygame
 # from module import function_hello, variable_player
 from constants import *
 
-# Define colors
-black = (0, 0, 0)
 
 def main():
-    # Add your main function code here
-    pass
+    pygame.init()
+    SCREEN_WIDTH = 1280
+    SCREEN_HEIGHT = 720
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
+        # 60 frames per second
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     print("Starting asteroids!")
     print("Screen width: 1280")
     print("Screen height: 720")
-    pygame.init()
-    SCREEN_WIDTH = 1280
-    SCREEN_HEIGHT = 720
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                exit()
-        screen.fill(black)
-        pygame.display.flip()
 main()
