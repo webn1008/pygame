57efabd9330b76c5cb7a9046d6c3b1b76b6a5a0d
Tony Melvin <webn1008@gmail.com>
2024-12-03 08:09:56 -0500

    Player and circleShape classes added

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pygame
 # from module import function_hello, variable_player
 from constants import *
-
+from circleshape import *
+from player import *
 
 def main():
     pygame.init()
@@ -10,12 +11,15 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    pygame.display.flip()
+    Player.draw(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2, radius = PLAYER_RADIUS)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        pygame.display.flip()
+        Player.draw(screen)
         # 60 frames per second
         dt = clock.tick(60) / 1000
 
--- player.py
+++ player.py
@@ -1,10 +1,32 @@
-def __init__(self, x, y, radius):
-    # we will be using this later
-    if hasattr(self, "containers"):
-        super().__init__(self.containers)
-    else:
-        super().__init__()
+import pygame
 
-    self.position = pygame.Vector2(x, y)
-    self.velocity = pygame.Vector2(0, 0)
-    self.radius = radius
+class Player(pygame.sprite.Sprite):
+    def __init__(self, x, y, radius, PLAYER_RADIUS):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        # we will be using this later
+        if hasattr(self, "containers"):
+            super().__init__(self.containers)
+        else:
+            super().__init__()
+
+        self.position = pygame.Vector2(x, y)
+        self.velocity = pygame.Vector2(0, 0)
+        self.radius = PLAYER_RADIUS
+        self.rotation = 0
+
+    # in the player class
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+
+    def draw(self, screen):
+        # Override from CircleShape
+        screen.fill("white")
+        pygame.draw.polygon(screen, "white", self.triangle(), width=2)
+
