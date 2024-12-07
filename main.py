bc09e2c57d5a11c5628c1a06019b5bece87ab325
Tony Melvin <webn1008@gmail.com>
2024-12-06 22:40:49 -0500

    Fully added group functionality. Cleaned up main and player files.

--- main.py
+++ main.py
@@ -1,32 +1,37 @@
 import pygame
-# from module import function_hello, variable_player
 from constants import *
-from circleshape import *
 from player import *
-updatable = pygame.sprite.Group()
-drawable = pygame.sprite.Group()
-Player.containers = (updatable, drawable)
+
+
 def main():
     pygame.init()
-    SCREEN_WIDTH = 1280
-    SCREEN_HEIGHT = 720
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     dt = 0
-    screen.fill("black")
-    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2, radius = 2, PLAYER_RADIUS = 20, PLAYER_SPEED = PLAYER_SPEED)
+
+    sprite = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        for player in updatable:
-            updatable.update(dt)
-        for player in drawable:
-            drawable.draw(screen)
-        # player.draw(screen)
-        # player.update(dt)
+        for sprite in updatable:
+            sprite.update(dt)
+
+        screen.fill("black")
+
+        for sprite in drawable:
+            sprite.draw(screen)
+
         pygame.display.flip()
+
         # 60 frames per second
         dt = clock.tick(60) / 1000
 
--- player.py
+++ player.py
@@ -1,23 +1,16 @@
 import pygame
+from constants import *
+from circleshape import CircleShape
 
-class Player(pygame.sprite.Sprite):
-    def __init__(self, x, y, radius, PLAYER_RADIUS, PLAYER_SPEED):
-        self.x = x
-        self.y = y
-        self.radius = radius
-        self.PLAYER_SPEED = PLAYER_SPEED
-        # we will be using this later
-        if hasattr(self, "containers"):
-            super().__init__(self.containers)
-        else:
-            super().__init__()
-
-        self.position = pygame.Vector2(x, y)
-        self.velocity = pygame.Vector2(0, 0)
-        self.radius = PLAYER_RADIUS
+class Player(CircleShape):
+    def __init__(self, x, y):
+        super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
 
-    # in the player class
+    def draw(self, screen):
+        # Override from CircleShape
+        pygame.draw.polygon(screen, "white", self.triangle(), 2)
+
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
@@ -26,24 +19,22 @@ class Player(pygame.sprite.Sprite):
         c = self.position - forward * self.radius + right
         return [a, b, c]
 
-    def draw(self, screen):
-        # Override from CircleShape
-        self.screen = screen
-        pygame.draw.polygon(screen, "white", self.triangle(), width=2)
-
-    def move(self, dt, PLAYER_SPEED):
+    def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_LEFT]:
-            self.rotation += dt * 300
+            self.rotate(dt)
         if keys[pygame.K_RIGHT]:
-            self.rotation -= dt * 300
+            self.rotate(-dt)
         if keys[pygame.K_UP]:
-            self.move(dt, self.PLAYER_SPEED)
+            self.move(dt)
         if keys[pygame.K_DOWN]:
-            self.move(dt, -self.PLAYER_SPEED)
+            self.move(-dt)
 
