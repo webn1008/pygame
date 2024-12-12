a1ef8e9541a0a33c8f144876e89956c0c00ea465
Tony Melvin <webn1008@gmail.com>
2024-12-12 16:48:32 -0500

    Added shot collision with asteroids.

--- main.py
+++ main.py
@@ -40,6 +40,10 @@ def main():
             if asteroid.collides_with(player):
                 print("Game over!")
                 return
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    asteroid.kill()
+                    shot.kill()
 
         screen.fill("black")
 
